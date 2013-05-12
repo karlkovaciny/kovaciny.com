807ad27f65005808d0e628b63ebbc83c74659465
Karl <fireslinger@gmail.com>
2013-05-12 11:54:46 -0500

    Indenting for readability

--- head.php
+++ head.php
@@ -80,16 +80,20 @@ require_once('functions.php');
 		</head>
 		
 		<body marginheight=0 marginwidth=0 leftmargin=0 topmargin=0">
-		<table width="100%" border=0 cellpadding=0 cellspacing=0 bgcolor="#6699CC" class="medium white"><tr><td width=219><a href="/"><img src="gfx/kovaciny.gif" border=0 width=199 height=60 hspace=10></a></td><td align="center">
-		<?php
-			echo "Welcome <b>$me</b>!";
-		?>
-		</td><td width=10>&nbsp;</td><td>
-		<form id="headerSearchForm" name="headerSearchForm" method="GET" action="conversationsearch.php">
-			<input id="headerSearchBox" name="q" class="copy headerSearchBox" type="text" title="Search" placeholder="Search" tabindex="10"><input id="headerSearchButton" type="submit" class="copy searchbutton" title="Click to search" value="">
-		</form></td>
-		<td width=10>&nbsp;</td>
-		</tr></table>
+		<table width="100%" border=0 cellpadding=0 cellspacing=0 bgcolor="#6699CC" class="medium white">
+			<tr>
+				<td width=219><a href="/"><img src="gfx/kovaciny.gif" border=0 width=199 height=60 hspace=10></a></td>
+				<td align="center">
+					<?php
+						echo "Welcome <b>$me</b>!";
+					?></td>
+				<td width=10>&nbsp;</td>
+				<td><form id="headerSearchForm" name="headerSearchForm" method="GET" action="conversationsearch.php">
+					<input id="headerSearchBox" name="q" class="copy headerSearchBox" type="text" title="Search" placeholder="Search" tabindex="10"><input id="headerSearchButton" type="submit" class="copy searchbutton" title="Click to search" value="">
+				</form></td>
+				<td width=10>&nbsp;</td>
+			</tr>
+		</table>
 		<table width="100%" border=0 cellpadding=0 cellspacing=0><tr valign="top" height=800><td bgcolor="#DDDDDD" style="padding:5px" width=130>
 		<p class="b" style="padding-top: 10px">Conversations</p>
 		<ul style="padding-left: 10px">
