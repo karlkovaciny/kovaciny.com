d1c7b25906f6989b92438b5eee8bbcc0fa56babb
Karl <fireslinger@gmail.com>
2014-01-24 01:19:50 -0600

    Undo!

--- convedit.php
+++ convedit.php
@@ -37,9 +37,7 @@ if (isset($_GET['action'])) {
 		}
 	}
 	if ($_GET['action'] == "new") {
-		//delete duplicate posts, apparently
-		$res= mysql_query("DELETE FROM `comments` WHERE `authorid` = '$newcomm_authorid' AND `conid` = '$conv_id' AND `comment` = '$newcomm'") or die("Could not update database: " . mysql_error()); 
-		
+		$res= mysql_query("DELETE FROM `comments` WHERE `authorid` = '$newcomm_authorid' AND `conid` = '$conv_id' AND `comment` = '$newcomm'") or die("Could not update database: " . mysql_error());
 		$res= mysql_query("INSERT INTO `comments` (`comid`, `inreplyto`, `conid`, `authorid`, `comment`, `createdate`, `changedate`) VALUES ('', '$newinreplyto', '$conv_id', '$newcomm_authorid', '$newcomm', $newcomm_posttime , $newcomm_posttime);") or die("Could not update comment database.");
 		if ($newcomm_posttime == "NOW()") {$res= mysql_query("UPDATE `conversations` SET `changedate` = NOW( ), `lastpostuserid` = '$newcomm_authorid', `lastpostusername` = '$newcomm_authorname' WHERE `authorid` = '$userid' AND `conid` = '$conv_id' LIMIT 1") or die("Could not update conversation database.");}
 		$updatecommentcount = true;
