a8dcd185d16f56d8abc8ad64c3a579a658ff3518
Karl <devnull@localhost>
2015-10-31 20:15:15 -0500

    version

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ define('DATABASE', '***REMOVED***');
 define('DB_PASSWORD', '***REMOVED***');
 define('HOST_NAME', 'http://kcom.kovaciny.com/k');
 define('DEBUG', true);
-define('RELEASE_VERSION', "1.0.2");
+define('RELEASE_VERSION', "1.0.3");
 date_default_timezone_set("America/Los_Angeles");
 ?>
