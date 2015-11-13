3ed60b966ec3108abde79fbf9f5960b09ccb4752
Karl <devnull@localhost>
2015-11-12 23:19:41 -0600

    duh!

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('DATABASE', '***REMOVED***');
 define('DB_PASSWORD', '***REMOVED***');
 define('HOST_NAME', 'http://kcom.kovaciny.com/k');
 define('DEBUG', true);
-define('RELEASE_VERSION', "1.0.10");
+define('RELEASE_VERSION', "1.0.11");
 date_default_timezone_set("America/Los_Angeles");
 define('MYSQL_DATETIME_FORMAT', 'Y-m-d H:i:s');
 $tz = -12;
