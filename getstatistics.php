888005e8c5ab56fff1587c8c5c7fe4007ac6334d
Noumenon72 <fireslinger@gmail.com>
2016-02-13 15:25:53 -0600

    Removing security-sensitive files for upload (also removed from commit history with BFG Repo-Cleaner)

--- getstatistics.php
+++ getstatistics.php
@@ -1,16 +1,7 @@
 <?php
-	// db Connection
-	$db = mysql_connect('localhost', '***REMOVED***', '***REMOVED***'); //no error, yes connected
-	if (!$db) {
-		die('Not connected : ' . mysql_error());
-	}
+    require_once('config.php');
+    require_once('dbconnect.php');
 
-	$db_selected = mysql_select_db ("***REMOVED***", $db);
-	if (!$db_selected) {
-		die ('Can\'t select db : ' . mysql_error());
-	}
-	$tz = -12;
-	
 	//get a list of the usernames and ids 
 	$res_users = mysql_query ("SELECT `userid`, `username` FROM `users`") or die ("Error getting usernames: " . mysql_error() . "<br />");
 	while ($row=mysql_fetch_array($res_users)) {
